7f21cc8d66b179e48107d999bfc6a1c3e106cfe7
JAINAM2510 <jainam.pavasiya2510@gmail.com>
2021-05-02 12:20:43 -0400

    adding main class to project
    adding title and description

--- Car_Rental/Program.cs
+++ Car_Rental/Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace Car_Rental
+namespace C_Sharp_Assignment_bittu
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            #region Description
+            Console.WriteLine("------------------------------------------");
+            Console.WriteLine("|               CAR RENTAL               |");
+            Console.WriteLine("------------------------------------------");
+            Console.Write("This application is about renting a car\n" +
+                "CAR RENTAL makes easy access to \n" +
+                "check all available car that can be rented.\n" +
+                "This application collects user data such as \n" +
+                "i)Username\nii)Age\niii)DrivingLiscense No\n");
+            Console.WriteLine("====WARNING!====\nENTER DATA IN SEQUNTIAL ORDER\nDO NOT REPEAT THE PROCESS\nIF THE DATA INPUT IS WRONGLY ENTERED PRESS 7 TO EXIT\n");
+
+            #endregion
+            Console.WriteLine("TYPE START TO BEGIN PROCESS");
+            carRentals callClassCarrentals = new carRentals(Console.ReadLine());
         }
     }
-}
